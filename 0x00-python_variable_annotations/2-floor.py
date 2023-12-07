471da1a07a9f6821c97888498488ffc84eb37f76
reggyshicky <reginahshikanda@gmail.com>
2023-12-07 15:55:18 +0300

    floor of a float, math.floor()

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Typed-annotations
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function that calculates the floor of an
+    int
+    """
+    return math.floor(n)
