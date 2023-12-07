9a3a3744f77dfd3c601e28657286606ed4892da6
reggyshicky <reginahshikanda@gmail.com>
2023-12-07 18:20:08 +0300

    duck typing

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Duck Typing
+"""
+from typing import Union, Sequence, Any
+
+
+def safe_first_element(lst: Sequence[any]) -> Union[any, None]:
+    """Safely returns the first element of a sequence of None
+    if the sequence is empty
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
