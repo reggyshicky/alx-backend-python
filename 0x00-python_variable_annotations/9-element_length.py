6523631f02db20be3a1eaac4a1dd4e7839e06f98
reggyshicky <reginahshikanda@gmail.com>
2023-12-07 18:11:06 +0300

    Iterables and Sequence - Generic concept

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,7 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    function that return a tuple which contain Sequence
+    """
     return [(i, len(i)) for i in lst]
