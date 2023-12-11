4421fce3b8de84eee4af242544c720b6d3d60e8d
reggyshicky <reginahshikanda@gmail.com>
2023-12-11 14:42:09 +0300

    total amount of time taken for wait_n

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-
+Measure runtijme
 """
-wait_n = __import__('1-concurrent_coroutines').wait_n
-import asyncio
 import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
